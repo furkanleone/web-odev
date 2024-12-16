81c2167c6d7039b929e23c211cd222bc9b988120
furkanleone <120941660+furkanleone@users.noreply.github.com>
2024-12-16 14:57:37 +0300

    create 4
    salon modelimi oluşturdunm

---
+++ Salon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace KuaforYonetim.Models
+{
+    public class Salon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string WorkingHours { get; set; }
+        public string Image { get; set; }
+       
+
+
+    }
+}
